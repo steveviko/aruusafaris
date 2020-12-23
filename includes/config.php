0626f3d50b5b7d2ca26e8bdffab7cf04716a9c72
steven opio <stevenviko@gmail.com>
2020-12-23 08:50:26 +0300

    Add DB Config

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','tours');
+define('DB_HOST','us-cdbr-east-02.cleardb.com');
+define('DB_USER','bcbb69c6374e89');
+define('DB_PASS','d17696e4');
+define('DB_NAME','heroku_b08175d2f44f053');
 // Establish database connection.
+//mysql://bcbb69c6374e89:d17696e4@us-cdbr-east-02.cleardb.com/heroku_b08175d2f44f053?reconnect=true
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
